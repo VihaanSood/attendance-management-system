4b338468c1c701e33e22b28f65073221fa8d2c9e
Vihaan Sood <vihaansood2006@gmail.com>
2026-03-23 20:48:48 +0530

    fix prisma permission final

--- scripts/migrate-and-start.js
+++ scripts/migrate-and-start.js
@@ -2,10 +2,11 @@
 const { execSync } = require('child_process')
 
 console.log('Running database migrations...')
-execSync('npx prisma migrate deploy', { stdio: 'inherit' })
+execSync('node node_modules/prisma/build/index.js migrate deploy', { stdio: 'inherit' })
+
 
 console.log('Generating Prisma client...')
-execSync('npx prisma generate', { stdio: 'inherit' })
+execSync('node node_modules/prisma/build/index.js generate', { stdio: 'inherit' })
 
 console.log('Starting server...')
 require('./src/server.js')
